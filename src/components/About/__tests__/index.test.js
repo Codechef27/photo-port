d8c83e294178ec52f4534931371ad78abd9b1ad7
BradleyK <bradleyjkennett@gmail.com>
2022-08-18 08:59:59 -0400

    test add

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {render, cleanup} from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+
+    it('renders', () => {
+        render(<About></About>)
+    });
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About></About>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+
+})
